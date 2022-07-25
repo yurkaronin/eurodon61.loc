a606531e0a6f885adbf0accffd5f4b689cc0a4bb
Yuriy Savchenko <47382042+yurkaronin@users.noreply.github.com>
2022-07-25 15:09:15 +0300

    Правки адаптива на 1024

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 let menuSearch = document.querySelector('.js-header-menu-search');
+let headerSearch = document.querySelector('.js-button-search');
+
 
 if(menuSearch) {
   menuSearch.onclick = function() {
@@ -9,4 +11,15 @@ if(menuSearch) {
       document.body.classList.add('header-search-active');
     }
   };
-}
+};
+
+if(headerSearch) {
+  headerSearch.onclick = function() {
+    console.log("wow");
+    if (document.body.classList.contains('header-search-active')) {
+      document.body.classList.remove('header-search-active');
+    } else {
+      document.body.classList.add('header-search-active');
+    }
+  };
+};
